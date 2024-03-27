17c87f96addd1341dde645f061aa81532cf50717
rjdalusung <82473422+rjdalusung@users.noreply.github.com>
2024-03-27 13:39:04 -0400

    Add files via upload

--- ChatClient.py
+++ ChatClient.py
@@ -1,32 +1,35 @@
 #ChatClient.py
 
 from socket import *
+import string
 import sys
 
-
+def signal_handler(sig, frame):
+    c.close()
+    sys.exit(0)
 args = sys.argv
 if len(args) != 5:
     print("Usage: python ChatClient.py HOSTNAME PORT NICKNAME CLIENT_ID")
     exit()
-
 HOST = args[1]
 PORT = int(args[2])
 NICKNAME = args[3]
 CLIENT_ID = int(args[4])
-
 #Create the socket and connect
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((HOST,PORT))
-
-#Client input
-clientString = input("Enter Message:\n")
-
-#Send the client input to server
-clientSocket.send(clientString.encode())
-
-#Read line from Server
-serverString = clientSocket.recv(1024)
-serverString = serverString.decode()
-print("Server:", serverString)
-
-clientSocket.close()
+c = socket(AF_INET, SOCK_STREAM)
+c.connect((HOST,PORT))
+c.send(NICKNAME.encode())
+while True:
+    #send the client info to the server
+    
+    # c.send(CLIENT_ID.encode())
+    #Client input
+    clientString = input("Enter Message:\n\n")
+    if clientString.upper() == "DISCONNECT":
+        c.close()
+    #Send the client input to server
+    c.send(clientString.encode())
+    print("\n\n" + sString)
+    #Read messages from Server
+    sString = c.recv(1024)
+    sString = sString.decode()
